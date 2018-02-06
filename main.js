bf00936edf1b3554484a8ac179f5e7200b7e7a0a
chinhnb <bachinh2105@gmail.com>
2018-02-06 22:06:18 +0700

    1

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-//const socket=io('https://stream1005.herokuapp.com/');
-const socket=io('http://localhost:3000/');
+const socket=io('https://stream1005.herokuapp.com/');
+//const socket=io('http://localhost:3000/');
 
 $('#div-chat').hide();
 
